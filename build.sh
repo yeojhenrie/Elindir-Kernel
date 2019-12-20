cc0b9dc5522c30f6d6d57675146dd65b3ddf0710
Krittin Kalra <androbada525@gmail.com>
2019-12-20 20:57:25 +0530

    Updated configs and patches to Elindir v2
    Conflicts:
    	oldjackfix.patch

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ KERNEL_DIR=$PWD
 Anykernel_DIR=$KERNEL_DIR/AnyKernel3/
 DATE=$(date +"%d%m%Y")
 TIME=$(date +"-%H.%M.%S")
-KERNEL_NAME="Elindir-v1"
+KERNEL_NAME="Elindir-v2"
 DEVICE="-kenzo-"
 FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$TIME"
 
